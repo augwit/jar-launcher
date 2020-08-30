67975a7f4263562cf7fd705249da5e28e47bc33a
Benjamin Qin <benjamin.qin@gmail.com>
2020-08-30 23:02:50 +0900

    完善 jar-launcher，增加 Java 环境兼容性

--- jar-launcher.sh
+++ jar-launcher.sh
@@ -3,9 +3,13 @@
 # company  :Augwit
 # author   :Benjamin Qin
 # email    :benjamin.qin@augwit.com
-# usage    :
-#   bash jar-launcher.sh [command]
-#   command: start/stop/restart to start, stop or restart the java application.
+# desc     :start, stop or restart a jar/war application.
+# usage    :bash jar-launcher.sh (start | stop | restart)
+#
+# Required ENV vars:
+# ------------------
+#   JAVA_HOME - location of a JDK home dir
+#
 #==============================================================================
 
 #You can change the config file name if you want.
@@ -21,23 +25,28 @@ echox() {
   fi
 }
 
+if [[ $# -lt 1 ]] ; then
+  echo "USAGE: $0 (start | stop | restart)"
+  exit 1
+fi
+
 if command -v realpath &> /dev/null
 then
   BASE_DIR=$(dirname $(realpath "$0"))
 else
-  echox "\033[1;31mError: The realpath command was not found, please install it.\033[0m"
-  echox "\033[1;32mIf you are under macOS, please try to install coreutils with homebrew.\033[0m"
-  exit
+  echox "\033[1;31mError: The realpath command was not found, please install it.\033[0m"  >&2
+  echox "\033[1;32mIf you are under macOS, please try to install coreutils with homebrew.\033[0m"  >&2
+  exit 2
 fi
 
 if [ -z "$BASE_DIR" ]; then
-  echox "\033[1;31mError: Cannot locate the directory where I (the bash script file) live.\033[0m"
-  exit
+  echox "\033[1;31mError: Cannot locate the directory where I (the bash script file) live.\033[0m"  >&2
+  exit 3
 fi
 
 if [ ! -f $BASE_DIR/$CONFIG_FILE_NAME ]; then
-  echox "\033[1;31mError: Cannot locate such configuration file:"
-  echox "$BASE_DIR/$CONFIG_FILE_NAME\033[0m"
+  echox "\033[1;31mError: Cannot locate such configuration file:"  >&2
+  echox "$BASE_DIR/$CONFIG_FILE_NAME\033[0m"  >&2
   echox "\n\033[1;32m#You need to define variables in $CONFIG_FILE_NAME
   \n#Here is an example:\033[0m"
   echo "APPLICATION_DISPLAY_NAME=\"Augwit Example Java Application\"
@@ -45,7 +54,7 @@ JAR_FILE_NAME=\"hello-world-0.0.1-SNAPSHOT.jar\"
 JAVA_COMMAND_ARGS=\"example-arg1 example-arg2\"
 LOG_OUTPUT_FILE_NAME=\"hello-world.log\"
   "
-  exit
+  exit 4
 fi
 
 source $BASE_DIR/$CONFIG_FILE_NAME
@@ -53,22 +62,116 @@ source $BASE_DIR/$CONFIG_FILE_NAME
 PATH_TO_JAR=$BASE_DIR/$JAR_FILE_NAME
 
 if [ ! -f $PATH_TO_JAR ]; then
-  echox "\033[1;31mError: Cannot locate java application:"
-  echox "$PATH_TO_JAR\033[0m"
-  exit
+  echox "\033[1;31mError: Cannot locate java application:" >&2
+  echox "$PATH_TO_JAR\033[0m" >&2
+  exit 5
 fi
 
 start_jar()
 {
   PID=$(ps -ef | grep $PATH_TO_JAR | grep -v 'grep' | awk '{print $2}')
-  if [ -z "$PID" ]; then
-       echo "Starting $APPLICATION_DISPLAY_NAME ..."
-       nohup java -jar $PATH_TO_JAR $JAVA_COMMAND_ARGS > $BASE_DIR/$LOG_OUTPUT_FILE_NAME &
-                   PID=$(echo $!)
-       sleep 1
-       echo "$APPLICATION_DISPLAY_NAME started in process $PID ..."
+  if [ ! -z "$PID" ]; then
+    echo "$APPLICATION_DISPLAY_NAME is already running in process $PID."
+    exit
+  fi
+
+  # OS specific support.  $var _must_ be set to either true or false.
+  cygwin=false;
+  darwin=false;
+  mingw=false
+  case "`uname`" in
+    CYGWIN*) cygwin=true ;;
+    MINGW*) mingw=true;;
+    Darwin*) darwin=true
+      # Use /usr/libexec/java_home if available, otherwise fall back to /Library/Java/Home
+      # See https://developer.apple.com/library/mac/qa/qa1170/_index.html
+      if [ -z "$JAVA_HOME" ]; then
+        if [ -x "/usr/libexec/java_home" ]; then
+          export JAVA_HOME="`/usr/libexec/java_home`"
+        else
+          export JAVA_HOME="/Library/Java/Home"
+        fi
+      fi
+      ;;
+  esac
+
+  if [ -z "$JAVA_HOME" ] ; then
+    if [ -r /etc/gentoo-release ] ; then
+      JAVA_HOME=`java-config --jre-home`
+    fi
+  fi
+
+  # For Cygwin, ensure paths are in UNIX format before anything is touched
+  if $cygwin ; then
+    [ -n "$JAVA_HOME" ] &&
+      JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
+  fi
+
+  # For Migwn, ensure paths are in UNIX format before anything is touched
+  if $mingw ; then
+    [ -n "$JAVA_HOME" ] &&
+      JAVA_HOME="`(cd "$JAVA_HOME"; pwd)`"
+  fi
+
+  if [ -z "$JAVA_HOME" ]; then
+    javaExecutable="`which javac`"
+    if [ -n "$javaExecutable" ] && ! [ "`expr \"$javaExecutable\" : '\([^ ]*\)'`" = "no" ]; then
+      # readlink(1) is not available as standard on Solaris 10.
+      readLink=`which readlink`
+      if [ ! `expr "$readLink" : '\([^ ]*\)'` = "no" ]; then
+        if $darwin ; then
+          javaHome="`dirname \"$javaExecutable\"`"
+          javaExecutable="`cd \"$javaHome\" && pwd -P`/javac"
+        else
+          javaExecutable="`readlink -f \"$javaExecutable\"`"
+        fi
+        javaHome="`dirname \"$javaExecutable\"`"
+        javaHome=`expr "$javaHome" : '\(.*\)/bin'`
+        JAVA_HOME="$javaHome"
+        export JAVA_HOME
+      fi
+    fi
+  fi
+
+  if [ -z "$JAVACMD" ] ; then
+    if [ -n "$JAVA_HOME"  ] ; then
+      if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
+        # IBM's JDK on AIX uses strange locations for the executables
+        JAVACMD="$JAVA_HOME/jre/sh/java"
+      else
+        JAVACMD="$JAVA_HOME/bin/java"
+      fi
+    else
+      JAVACMD="`which java`"
+    fi
+  fi
+
+  if [ ! -x "$JAVACMD" ] ; then
+    echo "Error: JAVA_HOME is not defined correctly." >&2
+    echo "  We cannot execute $JAVACMD" >&2
+    exit 6
+  fi
+
+  if [ -z "$JAVA_HOME" ] ; then
+    echo "Warning JAVA_HOME environment variable is not found."
+  fi
+  
+  # For Cygwin, switch paths to Windows format before running java
+  if $cygwin; then
+    [ -n "$JAVA_HOME" ] &&
+      JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
+  fi
+
+  echo "Starting $APPLICATION_DISPLAY_NAME ..."
+  nohup $JAVACMD -jar $PATH_TO_JAR $JAVA_COMMAND_ARGS >> $BASE_DIR/$LOG_OUTPUT_FILE_NAME &
+    PID=$(echo $!)
+  sleep 1
+  PID=$(ps -ef | grep $PATH_TO_JAR | grep -v 'grep' | awk '{print $2}')
+  if [ ! -z $PID ]; then
+    echo "$APPLICATION_DISPLAY_NAME started in process $PID."
   else
-       echo "$APPLICATION_DISPLAY_NAME is already running in process $PID ..."
+    echox "\033[1;31mSomething wrong when trying to start $APPLICATION_DISPLAY_NAME !\033[0m"
+    exit 6
   fi
 }
 
@@ -76,20 +179,15 @@ stop_jar()
 {
   PID=$(ps -ef | grep $PATH_TO_JAR | grep -v 'grep' | awk '{print $2}')
   if [ ! -z $PID ]; then
-         echo "$APPLICATION_DISPLAY_NAME in process $PID stopping ..."
-         kill $PID
-         PID=NULL
-         sleep 1
-         echo "$APPLICATION_DISPLAY_NAME stopped ..."
+    echo "$APPLICATION_DISPLAY_NAME in process $PID stopping ..."
+    kill $PID
+    PID=NULL
+    sleep 1
+    echo "$APPLICATION_DISPLAY_NAME stopped."
   else
-         echo "$APPLICATION_DISPLAY_NAME is not running ..."
+    echo "$APPLICATION_DISPLAY_NAME is not running."
   fi
 }
-
-if [[ $# -eq 0 ]] ; then
-    echo 'Please run this script with an argument: start, stop or restart'
-    exit 0
-fi
 
 case $1 in
   start)
